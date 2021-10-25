ca39b1b37e88739987c515fcf040e9905b51bb8f
QemerS <tu201806169@code.edu.az>
2021-10-26 02:33:57 +0400

    Add project files.

---
+++ IlkApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace IlkApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1-dən 100-ə qədər ədədləri console-da çap edin;
+            for (short x = 1; x <= 100; x++) {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
